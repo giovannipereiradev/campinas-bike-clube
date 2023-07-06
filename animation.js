ac8ee4d26b9b497b78861f0b6569fc195a758413
Giovanni Pereira <80793459+giovannipereiradev@users.noreply.github.com>
2023-07-06 12:13:35 -0400

    Âncoras Nav + About + Differentials 480px

--- animation.js
+++ animation.js
@@ -1,3 +1,8 @@
+//const nav =  document.querySelector("#nav")
+//const body =  document.querySelector("#body")
+//const main =  document.querySelector("#main")
+
+
 function clickMenu() {
     if (nav.style.display == 'flex') {
         nav.style.display = 'none'
@@ -8,4 +13,10 @@ function clickMenu() {
         body.style.overflow = 'hidden'
         main.style.filter = 'blur(3px)'
     }
+}
+
+function closeMenu() {
+    nav.style.display = 'none'
+    body.style.overflow = 'visible'
+    main.style.filter = 'blur(0px)'
 }
